99a42e3df29ea9e2ba544b6ee3de1b8864daf32a
MasterYousef <140777033+MasterYousef@users.noreply.github.com>
2024-10-30 09:12:10 +0300

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,8 @@ app.use(cors({
   credentials: true, 
 }))
 app.use(cookieParser())
-app.use(express.json());
+app.use(express.json({limit:"10mb"}));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(express.static(path.join(__dirname, "images")));
 routes(app);
 app.all("*", (req, res, next) => {
